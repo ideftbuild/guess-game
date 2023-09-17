156e71f7f1e628f6e828aadf620aaf271e6c51d4
akanSWE <a.akanSWE@gamil.com>
2023-09-17 14:12:53 +0100

    Add more prompt and guide

--- main.c
+++ main.c
@@ -5,20 +5,23 @@
  */
 int main(void)
 {
-	int guessCount, round, i;
+	int guessCount, round, i, score;
 
 	/*prompt*/
 	puts("Hi thanks, for joining the guess game!");
 	puts("In this game you have 6 trials to get the right number");
 	puts("Your guess shoud be within the range of 1 - 100");
-	puts("Enter -1 for hint");
+	printf("A hint will be shown to you to know if your guess ");
+	printf("is less than or greater than the answer, so watch out👀\n");
+	puts("Enter -1 to see more hint, note this can be used once for each round");
 	puts("Are you ready? Let go!💪💪");
 
 	for (i = 1; i <= 2; i++)
 		printf("↓↓↓↓↓↓↓↓\n");
 	printf("round (1)\n");
 
-	guessCount = 1, round = 1;
-	checkingGuess(guessCount, round);
+	guessCount = 1, round = 1, score = 0;
+	checkingGuess(&guessCount, round, score);
+
 	return (0);
 }
